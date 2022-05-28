a1dc114696c6e0c8bd71ba5ece895550202bdb64
Jerry Anselmi <jerry.anselmi@gmail.com>
2022-05-28 12:19:20 -0300

    Add the necessary middleware
    Signed-off-by: Jerry Anselmi <jerry.anselmi@gmail.com>

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,2 +1,6 @@
+import cors from "cors"
+import helmet from "helmet"
+import compression from "compression"
 import httpLogger from "./http-logger"
-export default [httpLogger]
+
+export default [cors(), helmet(), compression(), httpLogger]
